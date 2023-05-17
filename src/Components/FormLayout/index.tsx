0964d6adbba62cffd2fbbc04c1e79bb052f5f180
murashou <ymurashou@1stquad.com>
2023-05-17 12:18:31 +0300

    col row styles

--- src/Components/FormLayout/index.tsx
+++ src/Components/FormLayout/index.tsx
@@ -1,26 +1,35 @@
 import styles from './styles.module.scss'
+import genericStyles from '../../App.module.scss'
 import { FormLayoutType } from '../../Types/FormLayoutType'
 
 const FormLayout = (props: FormLayoutType) => {
 
   return (
-    <div className={styles.col}>
-      <div className={styles.row_w_100}>
-        <div className={styles.breadcrumbs}>
-          {props.breadcrumbs.map(breadcrumb => (
-            <span><a href="#">{breadcrumb}</a>/</span>
-          ))}
-          {props.title}
-        </div>
-        <h1>
-          {props.title}
-        </h1>
-      </div>
 
-      <div className={styles.row_w_55_align_self_center}>
+    <>
+    <div className={genericStyles.row}>
+      <div className={[genericStyles.col_12, styles.breadcrumbs].join(' ')}>
+          {props.breadcrumbs.map(breadcrumb => (
+            <span><a href="#" className={genericStyles.link}>{breadcrumb}</a>/</span>
+          ))}
+          <span className={genericStyles.help_text}>{props.title}</span>
+          
+      </div>
+    </div>
+    <div className={genericStyles.row}>
+      <div className={[genericStyles.col_12, styles.page_title].join(' ')}>          
+         {props.title}
+      </div>
+    </div>
+    <div className={genericStyles.row}>
+      <div className={[genericStyles.col_lg_6, genericStyles.offset_lg_3, genericStyles.col_12, genericStyles.offset_12].join(' ')}>
+      {/* the same
+       <div className={[genericStyles.col_lg_6, genericStyles.offset_lg_3, genericStyles.col_md_12, genericStyles.offset_md_12, genericStyles.col_sm_12, genericStyles.offset_sm_12].join(' ')}> */}
         {props.children}
       </div>
     </div>
+    </>
+    
   )
 }
 
--- src/Components/SingIn/index.tsx
+++ src/Components/SingIn/index.tsx
@@ -1,4 +1,5 @@
 import styles from './styles.module.scss'
+import genericStyles from '../../App.module.scss'
 import FormLayout from '../FormLayout'
 import FormElement from '../FormElement'
 import FormButton from '../FormButton'
@@ -9,26 +10,43 @@ const SingIn = () => {
     <FormLayout
       title={'Sign In'}
       breadcrumbs={['Back to Home']}>
-      <form className={styles.singInBox}>
-        <FormElement
-          type={'text'}
-          value={''}
-          placeholder={'Your email'}
-          label={'Email'} />
-        <FormElement
+      <form className={[genericStyles.bordered_box, genericStyles.content_center].join(' ')}>
+      <div className={genericStyles.row}>
+         <div className={genericStyles.col_12}>
+             <FormElement
+              type={'text'}
+              value={''}
+              placeholder={'Your email'}
+              label={'Email'} />
+         </div>
+      </div>
+      <div className={genericStyles.row}>
+         <div className={genericStyles.col_12}>
+         <FormElement
           type={'text'}
           value={''}
           placeholder={'Your password'}
           label={'Password'} />
-        <div className={styles.singInBox_footer}>
-          <a href="#">Forgot password?</a>
-          <FormButton
-            text="Sign In" />
-          <span>
-            Don’t have an account?
-            <a href="#"> Sign Up</a>
-          </span>
-        </div>
+         </div>
+      </div>
+      <div className={genericStyles.row}>
+         <div className={genericStyles.col_12}>
+            <a href="#" className={[genericStyles.link, genericStyles.m_t_5].join(" ")}>Forgot password?</a>
+         </div>
+      </div>
+      <div className={genericStyles.row}>
+         <div className={genericStyles.col_12}>
+            <FormButton text="Sign In" />
+         </div>
+      </div>
+      <div className={genericStyles.row}>
+         <div className={genericStyles.col_12}>
+            <span className={[genericStyles.help_text, genericStyles.m_t_25].join(" ")}>Don’t have an account? <a href="#" className={genericStyles.link}> Sign Up</a></span>
+         </div>          
+      </div>
+      
+       
+      
       </form>
     </FormLayout >
   )
